5a2a147afbd0801069b93762aa392c00cc1aefa9
Ananay Joshi <ananayjo22@gmail.com>
2022-08-11 14:43:19 -0700

    changed to rapidAPI, have a JSON response

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-let request = request ('request')
+const request = require('request');
 
-let apiKey = '';
-let city = '';
-let url = '';
+const options = {
+  method: 'GET',
+  url: 'https://aerisweather1.p.rapidapi.com/forecasts/cairo,eg',
+  headers: {
+    'X-RapidAPI-Key': 'bc3bd9d191mshf23dc8d03de7b8ap16ab6djsn3430246c02cf',
+    'X-RapidAPI-Host': 'aerisweather1.p.rapidapi.com',
+    useQueryString: true
+  }
+};
 
-request(url, function (err, response, body){
-    if(err){
-        console.log('error:', error);
-    } else {
-        console.log('body:', body);
-    }
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
 });
-
